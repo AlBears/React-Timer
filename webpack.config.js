fc3b548a30a743fec363ec9c8268b4dcbb282047
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-26 11:58:35 +0300

    Countdown component

--- webpack.config.js
+++ webpack.config.js
@@ -27,9 +27,10 @@ module.exports = {
 			Navigation: 'app/components/Navigation.jsx',
 			Timer: 'app/components/Timer.jsx',
 			Countdown: 'app/components/Countdown.jsx',
-			Clock: 'app/components/Clock.jsx'
-			
-		}, 
+			Clock: 'app/components/Clock.jsx',
+			CountdownForm: 'app/components/CountdownForm.jsx'
+
+		},
 		extensions: ['', '.js', '.jsx']
 	},
 	module: {
